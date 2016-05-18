be4c4f20413badf27e5e89d5db8063d5a7227933
Marek <marekventur@gmail.com>
2016-05-18 17:15:57 +0100

    Spritesheet name is now its hash

--- test/unit/spritesheet.js
+++ test/unit/spritesheet.js
@@ -4,7 +4,7 @@ var Spritesheet = require("../../lib/spritesheet");
 var assert = require("chai").assert;
 
 describe("Spritesheet", function () {
-    var spritesheet, bin, groupConfig, config, cache, cachePath, imageProcessor;
+    var spritesheet, bin, groupConfig, config, cache, cachePath, imageProcessor, scaledGroup;
 
     beforeEach(function() {
         bin = {
@@ -24,42 +24,42 @@ describe("Spritesheet", function () {
         cache = {};
         cachePath = "/some/path";
         imageProcessor = {};
-        spritesheet = new Spritesheet("test-spritesheet", bin, groupConfig, config, cache, cachePath, imageProcessor);
+        scaledGroup = { groupHash: "abcd" };
+        spritesheet = new Spritesheet(scaledGroup, bin, groupConfig, config, cache, cachePath, imageProcessor);
     });
 
     context("#calculateHash", () => {
-        let originalHash;
-        beforeEach(() => {
-            originalHash = spritesheet.calculateHash();
-        });
-
         it("stays the same if no change is made", function() {
-            let otherSpritesheet = new Spritesheet("test-spritesheet", bin, groupConfig, config, cache, cachePath, imageProcessor);
-            assert.equal(originalHash, otherSpritesheet.calculateHash());
+            let otherSpritesheet = new Spritesheet(scaledGroup, bin, groupConfig, config, cache, cachePath, imageProcessor);
+            assert.equal(spritesheet.hash, otherSpritesheet.hash);
         });
 
         it("changes when path of a rect changes", function() {
             bin.rects[0].data.path = "/one/sprite/changed";
-            assert.notEqual(originalHash, spritesheet.calculateHash());
+            let otherSpritesheet = new Spritesheet(scaledGroup, bin, groupConfig, config, cache, cachePath, imageProcessor);
+            assert.notEqual(spritesheet.hash, otherSpritesheet.hash);
         });
 
         it("changes when scaledSprites is added", function() {
             bin.rects.push({data: {path: "foo/bar"}})
-            assert.notEqual(originalHash, spritesheet.calculateHash());
+            let otherSpritesheet = new Spritesheet(scaledGroup, bin, groupConfig, config, cache, cachePath, imageProcessor);
+            assert.notEqual(spritesheet.hash, otherSpritesheet.hash);
         });
 
         it("changes when extension changes", function() {
             groupConfig.jpeg = false;
-            assert.notEqual(originalHash, spritesheet.calculateHash());
+            let otherSpritesheet = new Spritesheet(scaledGroup, bin, groupConfig, config, cache, cachePath, imageProcessor);
+            assert.notEqual(spritesheet.hash, otherSpritesheet.hash);
         });
 
         it("changes when quality changes", function() {
             groupConfig.quality = 75;
-            assert.notEqual(originalHash, spritesheet.calculateHash());
+            let otherSpritesheet = new Spritesheet(scaledGroup, bin, groupConfig, config, cache, cachePath, imageProcessor);
+            assert.notEqual(spritesheet.hash, otherSpritesheet.hash);
         });
     });
 
     it("has the correct basename", function() {
-        assert.equal(spritesheet.getBasename(), "test-spritesheet.jpeg");
+        assert.equal(spritesheet.basename, "1eca9bde9b9a851b574a7d1983576d90f118f3d2.jpeg");
     });
 });
