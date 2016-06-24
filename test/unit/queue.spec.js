425187c1232efc95fa6cf433c83b1adb5443e1ed
Marek <marekventur@gmail.com>
2016-06-24 16:49:08 +0100

    Move spinner into special log class

--- test/unit/queue.spec.js
+++ test/unit/queue.spec.js
@@ -12,16 +12,9 @@ describe("Queue", () => {
         let generator1 = sinon.stub().returns(new Promise(r => resolve1 = r));
         let generator2 = sinon.stub().returns(new Promise(r => resolve2 = r));
 
-        let done1 = false;
-        let promise1 = queue.add(generator1).then(() => done1 = true);
+        let promise1 = queue.add(generator1);
         let promise2 = queue.add(generator2);
 
-        let done = false;
-        let promise = queue.drainWhile("something", Promise.all([promise1, promise2]), false)
-            .then(() => done = true);
-
-        expect(done1).to.equal(false);
-        expect(done).to.equal(false);
         expect(queue.remaining).to.equal(2);
 
         expect(generator1.callCount).to.equal(1);
@@ -31,8 +24,6 @@ describe("Queue", () => {
 
         return promise1
         .then(() => {
-            expect(done1).to.equal(true);
-            expect(done).to.equal(false);
             expect(queue.remaining).to.equal(1);
 
             expect(generator1.callCount).to.equal(1);
@@ -44,7 +35,6 @@ describe("Queue", () => {
         })
         .then(() => {
             expect(queue.remaining).to.equal(0);
-            return promise;
         });
     });
 });
